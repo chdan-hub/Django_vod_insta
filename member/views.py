ed70a3dc70929422937763993a24115d7d386ea1
chdan-hub <ksmii358@gmail.com>
2025-10-01 01:53:01 -0700

    add profile detail

--- member/views.py
+++ member/views.py
@@ -5,7 +5,7 @@ from django.core.signing import TimestampSigner, SignatureExpired
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.views.generic import FormView, DetailView
 
 from member.forms import SignUpForm, LoginForm
 from utils.email import send_email
@@ -14,68 +14,75 @@ User = get_user_model()
 
 
 class SignupView(FormView):
-    template_name = 'auth/signup.html'
-    form_class = SignUpForm
-    success_url = reverse_lazy('signup_done')
+  template_name = 'auth/signup.html'
+  form_class = SignUpForm
+  success_url = reverse_lazy('signup_done')
 
-    def form_valid(self, form):
-        user = form.save()
-        # 이메일 발송
-        signer = TimestampSigner()
-        signed_user_email = signer.sign(user.email)
-        signer_dump = signing.dumps(signed_user_email)
-        # print(signer_dump)
-        #
-        # decoded_user_email = signing.loads(signer_dump)
-        # print(decoded_user_email)
-        # email = signer.unsign(decoded_user_email, max_age=60 * 30)
-        # print(email)
-        url = f'{self.request.scheme}://{self.request.META['HTTP_HOST']}/verify/?code={signer_dump}'
-        if settings.DEBUG:
-            print(url)
-        else:
-            subject = '[Pystagram]이메일 인증을 완료해주세요.'
-            message = f'다음 링크를 클릭해주세요. <br><a href="{url}">{url}</a>'
+  def form_valid(self, form):
+    user = form.save()
+    # 이메일 발송
+    signer = TimestampSigner()
+    signed_user_email = signer.sign(user.email)
+    signer_dump = signing.dumps(signed_user_email)
+    # print(signer_dump)
+    #
+    # decoded_user_email = signing.loads(signer_dump)
+    # print(decoded_user_email)
+    # email = signer.unsign(decoded_user_email, max_age=60 * 30)
+    # print(email)
+    url = f'{self.request.scheme}://{self.request.META['HTTP_HOST']}/verify/?code={signer_dump}'
+    if settings.DEBUG:
+      print(url)
+    else:
+      subject = '[Pystagram]이메일 인증을 완료해주세요.'
+      message = f'다음 링크를 클릭해주세요. <br><a href="{url}">{url}</a>'
 
-            send_email(subject, message, user.email)
+      send_email(subject, message, user.email)
 
-        return render(
-            self.request,
-            template_name='auth/signup_done.html',
-            context={'user': user},
-        )
+    return render(
+      self.request,
+      template_name='auth/signup_done.html',
+      context={'user': user},
+    )
+
 
 def verify_email(request):
-    code = request.GET.get('code', '')
+  code = request.GET.get('code', '')
 
-    signer = TimestampSigner()
-    try:
-        decoded_user_email = signing.loads(code)
-        email = signer.unsign(decoded_user_email, max_age=60 * 30)
-    except (TypeError, SignatureExpired):
-        return render(request, 'auth/not_verified.html')
+  signer = TimestampSigner()
+  try:
+    decoded_user_email = signing.loads(code)
+    email = signer.unsign(decoded_user_email, max_age=60 * 30)
+  except (TypeError, SignatureExpired):
+    return render(request, 'auth/not_verified.html')
 
-    user = get_object_or_404(User, email=email, is_active=False)
-    user.is_active = True
-    user.save()
-    from django.urls import reverse
-    return redirect(reverse('login'))
-    # return render(request, 'auth/email_verified_done.html', {'user': user})
+  user = get_object_or_404(User, email=email, is_active=False)
+  user.is_active = True
+  user.save()
+  from django.urls import reverse
+  return redirect(reverse('login'))
+  # return render(request, 'auth/email_verified_done.html', {'user': user})
 
 
 class LoginView(FormView):
-    template_name = 'auth/login.html'
-    form_class = LoginForm
-    success_url = reverse_lazy('main')
+  template_name = 'auth/login.html'
+  form_class = LoginForm
+  success_url = reverse_lazy('main')
 
-    def form_valid(self, form):
-        user = form.user
-        login(self.request, user)
+  def form_valid(self, form):
+    user = form.user
+    login(self.request, user)
 
-        next_page = self.request.GET.get('next')
-        if next_page:
-            return HttpResponseRedirect(next_page)
+    next_page = self.request.GET.get('next')
+    if next_page:
+      return HttpResponseRedirect(next_page)
 
-        return HttpResponseRedirect(self.get_success_url())
+    return HttpResponseRedirect(self.get_success_url())
 
 
+class UserProfileView(DetailView):
+  model = User
+  template_name = 'profile/detail.html'
+  slug_field = 'nickname'
+  slug_url_kwarg = 'slug'
+  queryset = User.objects.all().prefetch_related('post_set', 'post_set__images')
